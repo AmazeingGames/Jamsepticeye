5086dee1f94b7da80b51e274e78cf0614406fb05
AmazeingGames <AmazeingGameDesign@gmail.com>
2025-10-05 18:28:28 -0700

    Small UI Fixes

--- Jamsepticeye/Assets/Jamsepticeye/Scripts/Menus/UIButton.cs
+++ Jamsepticeye/Assets/Jamsepticeye/Scripts/Menus/UIButton.cs
@@ -9,18 +9,47 @@ using UnityEngine.UI;
 // One of the few class responsible for conveying important changes in game state directly to the game manager
 public class UIButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public enum ButtonFunctionality { None, StartGame, OpenCredits, EnterMainMenu }
+
+    public static EventHandler<ButtonInteractEventArgs> ButtonInteractEventHandler;
+
+    [SerializeField] ButtonFunctionality myButtonFunctionality;
+
+    public class ButtonInteractEventArgs : EventArgs
+    {
+        public enum InteractType { PointerEnter, PointerExit, PointerClick }
+        public readonly InteractType myInteractType;
+
+        public ButtonInteractEventArgs(InteractType myInteractType)
+        {
+            this.myInteractType = myInteractType;
+        }
+    }
+
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        throw new NotImplementedException();
+        ButtonInteractEventHandler?.Invoke(this, new(ButtonInteractEventArgs.InteractType.PointerClick));
+
+        switch (myButtonFunctionality)
+        {
+            case ButtonFunctionality.StartGame:
+                break;
+            case ButtonFunctionality.OpenCredits:
+                break;
+            case ButtonFunctionality.EnterMainMenu:
+                break;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        throw new NotImplementedException();
+        ButtonInteractEventHandler?.Invoke(this, new(ButtonInteractEventArgs.InteractType.PointerEnter));
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        throw new NotImplementedException();
+        ButtonInteractEventHandler?.Invoke(this, new(ButtonInteractEventArgs.InteractType.PointerExit));
     }
 }
