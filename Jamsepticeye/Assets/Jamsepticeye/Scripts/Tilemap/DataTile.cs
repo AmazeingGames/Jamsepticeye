7ae166b65143f30325fb1d632fff30c478b58af2
AmazeingGames <AmazeingGameDesign@gmail.com>
2025-10-06 18:55:37 -0700

    Created New Tiles
    Created new tile palettes and scriptable object tiles to draw with. Created a script to batch create tiles from a given sprite

--- Jamsepticeye/Assets/Jamsepticeye/Scripts/Tilemap/DataTile.cs
+++ Jamsepticeye/Assets/Jamsepticeye/Scripts/Tilemap/DataTile.cs
@@ -4,7 +4,8 @@ using UnityEngine.Tilemaps;
 
 public class DataTile : Tile
 {
-    public enum Category { Grass, Dirt, Stone, Wood }
+    public enum Category { None = 0, Grass = 1, Dirt = 2, Stone = 3}
+    [Header("Custom Data")]
     [field: SerializeField] public Category MyTile { get; private set; }
     
 }
---
+++ Jamsepticeye/Assets/Jamsepticeye/Scripts/Tools/TileCreator.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+using System.IO;
+using System.Linq;
+
+public class TileCreator
+{
+    [MenuItem("Assets/Batch Create/DataTiles")]
+    public static void CreateTilesFromMultipleSprites()
+    {
+        // Get the selected Texture2D assets
+        var textures = Selection.GetFiltered<Texture2D>(SelectionMode.Assets);
+
+        foreach (var texture in textures)
+        {
+            // Load all sub-sprites from the texture (must be Sprite Mode Multiple)
+            string assetPath = AssetDatabase.GetAssetPath(texture);
+            Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath(assetPath).OfType<Sprite>().ToArray();
+
+            // For each sub-sprite create your ScriptableObject
+            foreach (var sprite in sprites)
+            {
+                if (sprite == null) continue;
+
+                // Create instance of your ScriptableObject
+                DataTile tileData = ScriptableObject.CreateInstance<DataTile>();
+                tileData.sprite = sprite;
+
+                string directory = Path.GetDirectoryName(assetPath);
+                string assetName = sprite.name + "_TileData.asset";
+                string savePath = Path.Combine(directory, assetName);
+
+                AssetDatabase.CreateAsset(tileData, savePath);
+            }
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        Debug.Log("Created tiles from multiple sprites.");
+    }
+}
