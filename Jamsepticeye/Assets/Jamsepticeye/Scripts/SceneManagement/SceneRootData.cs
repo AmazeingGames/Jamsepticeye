c907fec7be43358b10ab878e524d906735460065
AmazeingGames <AmazeingGameDesign@gmail.com>
2025-10-06 18:19:20 -0700

    Set Up Scene Roots
    Added the scene root script to each scene root in the hierarchy and created and added root data for each of them.

--- Jamsepticeye/Assets/Jamsepticeye/Scripts/SceneManagement/SceneRootData.cs
+++ Jamsepticeye/Assets/Jamsepticeye/Scripts/SceneManagement/SceneRootData.cs
@@ -1,11 +1,9 @@
 using FMODUnity;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SceneRootData", menuName = "Scriptable Objects/SceneRootData")]
+[CreateAssetMenu(fileName = "SceneRootData", menuName = "ScriptableObjects/SceneRootData")]
 public class SceneRootData : ScriptableObject
 {
-    [field: SerializeField] public FMODEvents.MusicType MyMusic { get; private set; }
-    [field: SerializeField] public FMODEvents.AmbType MyAmbience { get; private set; }
-    [field: SerializeField] public EventReference PlayerFootsteps { get; private set; }
-
+    [field: SerializeField] public FMODEvents.MusicType MyMusic { get; private set; } = FMODEvents.MusicType.None;
+    [field: SerializeField] public FMODEvents.AmbType MyAmbience { get; private set; } = FMODEvents.AmbType.None;
 }
--- Jamsepticeye/Assets/Jamsepticeye/Scripts/Tilemap/DataTile.cs
+++ Jamsepticeye/Assets/Jamsepticeye/Scripts/Tilemap/DataTile.cs
@@ -2,7 +2,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class DataTile : TileBase
+public class DataTile : Tile
 {
     public enum Category { Grass, Dirt, Stone, Wood }
     [field: SerializeField] public Category MyTile { get; private set; }
